a931c531b64a48ccf2930021fa3e1dd4e3c3f707
Bogdan Arabadzhi <bogdan.today@gmail.com>
2017-02-17 16:33:10 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function sum() {
-  // your solution
+module.exports = function sum(...n) {
+  n = n.reduce((p, c) => p + c, 0);
+  const fn = (...x) => sum(n + x.reduce((p, c) => p + c, 0));
+  fn.valueOf = () => n;
+  return fn;
 }
